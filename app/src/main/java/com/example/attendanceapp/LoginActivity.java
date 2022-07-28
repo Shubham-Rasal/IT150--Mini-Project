96f0ff3ec75e00cc9da156f0baccdaddd1feb9a6
Shubham-Rasal <bluequbits@gmail.com>
2022-07-28 14:05:16 +0530

    Done testing login user firebaseAuth

--- app/src/main/java/com/example/attendanceapp/LoginActivity.java
+++ app/src/main/java/com/example/attendanceapp/LoginActivity.java
@@ -11,11 +11,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     
     
     private Button login;
     private EditText email,password;
+
+
+    //iniatilizind firebse auth
+    //Auth
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +37,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeAsUpIndicator(R.drawable.arrow_back);
         actionBar.setDisplayHomeAsUpEnabled(true);
-        
+
+
         login =(Button) findViewById(R.id.login);
         email = (EditText) findViewById(R.id.emailId);
         password = (EditText) findViewById(R.id.password);
         
         login.setOnClickListener(this);
+
+
         
         
 
@@ -55,6 +68,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         if(v.getId()==R.id.login)
         {
+            String userEmail = email.getText().toString().trim();
+            String userPass = password.getText().toString().trim();
+
+            //sending credentials entered to validate to firebase
+            mAuth.signInWithEmailAndPassword("test@gmail.com","fker9jfs").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                        Toast.makeText(LoginActivity.this, "login failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+           
+        
+
+
 
         }
             
