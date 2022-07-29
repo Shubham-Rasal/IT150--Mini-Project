9b8d5b9bcfdd521891bf758907555ed0a7d2a51a
AbhishekSatpathy4848 <abhisheksatpathy.211it002@nitk.edu.in>
2022-07-29 15:35:09 +0530

    Made Student Class and wrote some JAVA Code

--- app/src/main/java/com/example/attendanceapp/RegistrationActivity.java
+++ app/src/main/java/com/example/attendanceapp/RegistrationActivity.java
@@ -3,12 +3,34 @@ package com.example.attendanceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    EditText name,password,confirmPassword,email;
+    Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        email=findViewById(R.id.Email);
+        name=findViewById(R.id.Name);
+        password=findViewById(R.id.Password);
+        confirmPassword=findViewById(R.id.ConfirmPassword);
+        registerButton=findViewById(R.id.Register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String EMAIL=email.getText().toString();
+                String NAME=name.getText().toString();
+                String PASSWORD=password.getText().toString();
+                String CONFIRM_PASSWORD=confirmPassword.getText().toString();
+                Student s=new Student(NAME,EMAIL,PASSWORD);
+
+            }
+        });
+
+
     }
 }
