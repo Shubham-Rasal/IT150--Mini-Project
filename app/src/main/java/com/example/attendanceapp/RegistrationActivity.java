32bd06283bce7403868002895e2965cda023bc47
AbhishekSatpathy4848 <abhisheksatpathy.211it002@nitk.edu.in>
2022-08-04 16:48:09 +0530

    Added Authentication Database to RegistrationActivity.java and accounted for faulty registrations by checking for NITK email ID etc.

--- app/src/main/java/com/example/attendanceapp/RegistrationActivity.java
+++ app/src/main/java/com/example/attendanceapp/RegistrationActivity.java
@@ -1,21 +1,29 @@
 package com.example.attendanceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 public class RegistrationActivity extends AppCompatActivity {
     EditText name,password,confirmPassword,email;
     Button registerButton;
     FirebaseDatabase Firebase;
     DatabaseReference student_databaseReference;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +40,43 @@ public class RegistrationActivity extends AppCompatActivity {
                 String NAME=name.getText().toString();
                 String PASSWORD=password.getText().toString();
                 String CONFIRM_PASSWORD=confirmPassword.getText().toString();
-                if(PASSWORD.equals(CONFIRM_PASSWORD)) {
-                    Student s = new Student(NAME, EMAIL, PASSWORD);
-                    Firebase = FirebaseDatabase.getInstance();
-                    student_databaseReference = Firebase.getReference("Students");
-                    student_databaseReference.push().setValue(s);
-                    Toast.makeText(RegistrationActivity.this, "Registered!!", Toast.LENGTH_SHORT).show();
+                if(PASSWORD.equals(CONFIRM_PASSWORD) && EMAIL.contains("nitk.edu.in")) {
+                    mAuth =FirebaseAuth.getInstance();
+
+                    mAuth.createUserWithEmailAndPassword(EMAIL, PASSWORD)
+                            .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+//                                        FirebaseUser user = mAuth.getCurrentUser();
+//                                        updateUI(user);
+                                        Student s = new Student(NAME, EMAIL, PASSWORD);
+                                        Firebase = FirebaseDatabase.getInstance();
+                                        student_databaseReference = Firebase.getReference("Students");
+                                        student_databaseReference.push().setValue(s);
+                                        Toast.makeText(RegistrationActivity.this, "Registered Successfully!!", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        if(PASSWORD.length()<6){
+                                            Toast.makeText(RegistrationActivity.this, "The password should have at least 6 characters", Toast.LENGTH_SHORT).show();
+                                        }
+                                        else {
+                                            Toast.makeText(RegistrationActivity.this, "User Already Exists", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                }
+                            });
+
+
                 }
                 else {
-                    Toast.makeText(RegistrationActivity.this, "Passwords don't match!!!", Toast.LENGTH_SHORT).show();
+                    if(!EMAIL.contains("nitk.edu.in")){
+                        Toast.makeText(RegistrationActivity.this, "Please enter the NITK Email ID", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(!PASSWORD.equals(CONFIRM_PASSWORD)){
+                        Toast.makeText(RegistrationActivity.this, "Passwords don't match!!!", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
             }
         });
