ffe4476ebf16a83fca98bc3ed0c4e9e041f7a737
Shubham-Rasal <bluequbits@gmail.com>
2022-08-09 12:17:51 +0530

    Added basic popup to individual class

--- app/src/main/java/com/example/attendanceapp/TeacherActivity.java
+++ app/src/main/java/com/example/attendanceapp/TeacherActivity.java
@@ -11,10 +11,16 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -84,7 +90,7 @@ public class TeacherActivity extends AppCompatActivity {
         cross = findViewById(R.id.cross);
         cardView = findViewById(R.id.cardView);
         cardView.setVisibility(View.INVISIBLE);
-        listView = findViewById(R.id.listView);
+        listView = findViewById(R.id.listView1);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Classes");
@@ -106,6 +112,13 @@ public class TeacherActivity extends AppCompatActivity {
                     }
                 }
                 MyAdapter myAdapter = new MyAdapter(TeacherActivity.this, R.layout.cardview, className);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(TeacherActivity.this, ""+id, Toast.LENGTH_SHORT).show();
+                        onButtonShowPopupWindowClick(view);
+                    }
+                });
                 listView.setAdapter(myAdapter);
             }
 
@@ -126,5 +139,40 @@ public class TeacherActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+
+
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window token
+        popupWindow.showAtLocation(view, Gravity.CENTER, 10, 0);
+        TextView popupText = (TextView) popupView.findViewById(R.id.popup_window);
+
+
+        popupText.setText("hello");
+
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
